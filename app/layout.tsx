1f98ff9868cad245954df3a693057ff7ab1fbc1f
javier Palencia <javi.web23@gmail.com>
2026-07-06 14:54:08 -0400

    fix: resolucion de conflictos y actualizacion forzada de favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,11 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Adventures Finder",
   description: "Directory for tours and adventures",
+  icons: {
+    icon: "/images/icono.png",
+    shortcut: "/images/icono.png",
+    apple: "/images/icono.png",
+  },
 };
 
 export default function RootLayout({
