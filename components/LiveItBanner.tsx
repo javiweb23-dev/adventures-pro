b95d97db40555c11c59428d490e095f9c7ccfc72
javier Palencia <javi.web23@gmail.com>
2026-07-14 09:39:27 -0400

    feat: banner live it con efecto sticky inteligente al hacer scroll

--- components/LiveItBanner.tsx
+++ components/LiveItBanner.tsx
@@ -1,11 +1,38 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Image from "next/image";
 
 const LIVE_IT_URL =
   "https://play.google.com/store/apps/details?id=com.gallusgolf.c1881.android.liveit";
 
 export default function LiveItBanner() {
+  const [hidden, setHidden] = useState(false);
+
+  useEffect(() => {
+    let lastScrollY = window.scrollY;
+
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      const scrollingDown = currentScrollY > lastScrollY;
+      const pastThreshold = currentScrollY > 80;
+
+      setHidden(scrollingDown && pastThreshold);
+      lastScrollY = currentScrollY;
+    };
+
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <section className="mx-auto my-12 w-full max-w-7xl px-4 md:px-10 lg:px-12">
+    <div
+      className={`fixed bottom-4 left-1/2 z-50 w-[95%] max-w-5xl -translate-x-1/2 shadow-2xl transition-all duration-300 ease-in-out ${
+        hidden
+          ? "translate-y-[150%] opacity-0"
+          : "translate-y-0 opacity-100"
+      }`}
+    >
       <a
         href={LIVE_IT_URL}
         target="_blank"
@@ -20,6 +47,6 @@ export default function LiveItBanner() {
           className="h-auto w-full rounded-2xl object-cover"
         />
       </a>
-    </section>
+    </div>
   );
 }
